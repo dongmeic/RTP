765da723bd1a3237ec93e7308cfc37f8d60e0bcd
CHEN Dongmei <DCHEN@lcog.org>
2021-01-07 15:31:47 -0800

    read data from SQL server

--- read_geodata.py
+++ read_geodata.py
@@ -3,4 +3,15 @@ The script connects and reads data from RLID gepspatial database
 
 '''
 import pyodbc
+import pandas as pd
 
+cnxn = pyodbc.connect("Driver={SQL Server};"
+                      "Server=rliddb.int.lcog.org,5433;"
+                      "Database=RLIDGeo;"
+                      "Trusted_Connection=yes;")
+
+df = pd.read_sql_query('select * from dbo.BikeFacility', cnxn)
+df.head()
+list(df)
+df.ftype.unique()
+df.groupby(['ftype','ftypedes']).size().reset_index().rename(columns={0:'count'})
