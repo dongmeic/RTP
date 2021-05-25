3039b595c0f18a508d51a3a1e0db7246ed4c8838
CHEN Dongmei <DCHEN@lcog.org>
2021-05-24 20:10:00 -0700

    review RTP projects

--- projlist/review_RTP.py
+++ projlist/review_RTP.py
@@ -3,6 +3,35 @@ import geopandas as gpd
 import re, fiona
 import numpy as np
 
+
+def matchID(rtpid_table, rtpid_layer):
+    newRTPid = [a for a in rtpid_table if a not in rtpid_layer]
+    missedRTPid = [a for a in rtpid_layer if a not in rtpid_table]
+    commonid = [a for a in rtpid_table if a in rtpid_layer]
+    return newRTPid, missedRTPid, commonid
+
+def getIDs(excel='Working DRAFT 2045 Project List.xlsx',
+           Tablepattern='Auto Constrained',
+           Layerpattern='Constrained_Roadway'):
+    sheetNames = getSheetnames(pattern=Tablepattern)
+    rtpid_table = []
+    for sheetName in sheetNames:
+        print(sheetName)
+        xl = pd.ExcelFile(excel)
+        df = xl.parse(sheetName)
+        if df.shape[0] != 0:
+            l = getRTPid(sheet_name=sheetName)[1]
+            rtpid_table += l
+    
+    layers = [item for item in getLayernames(pattern=Layerpattern) if 'P1' not in item]
+    rtpid_layer = []
+    for layer in layers:
+        print(layer)
+        l = LayerRTPid(layer = layer)
+        rtpid_layer += l
+    
+    return rtpid_table, rtpid_layer
+
 def LayerRTPid(gdb_file = r'T:\MPO\RTP\FY16 2040 Update\Data\RTP_2040_Data.gdb',
                layer='Constrained_Roadway_lines_P1'):
     gpdfile = gpd.read_file(gdb_file, layer=layer)
@@ -18,16 +47,17 @@ def getRTPid(excel='Working DRAFT 2045 Project List.xlsx',
              sheet_name='Auto Constrained - Arterial Lin'):
     xl = pd.ExcelFile(excel)
     df = xl.parse(sheet_name)
-    name = df['Name'][0].split(":")[1].lstrip()
-    df = df.drop(labels=0, axis=0)
-    rtplist = df['RTP #'].unique()
-    strlist = [item for item in rtplist if type(item) is str]
-    if len(strlist) > 0:
-        for s in strlist:
-            rtplist = rtplist[rtplist != s]
-    a = rtplist.astype(float)
-    b = a[~np.isnan(a)].astype(int).tolist()
-    res = [int(ele) for ele in b if ele > 0]
+    if df.shape[0] != 0:
+        name = df['Name'][0].split(":")[1].lstrip()
+        df = df.drop(labels=0, axis=0)
+        rtplist = df['RTP #'].unique()
+        strlist = [item for item in rtplist if type(item) is str]
+        if len(strlist) > 0:
+            for s in strlist:
+                rtplist = rtplist[rtplist != s]
+        a = rtplist.astype(float)
+        b = a[~np.isnan(a)].astype(int).tolist()
+        res = [int(ele) for ele in b if ele > 0]
     return name, res
 
 def getSheetnames(excel='Working DRAFT 2045 Project List.xlsx',
