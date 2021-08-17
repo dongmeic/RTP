3a35f7b2d01992eab0ca208bb2d2985f32a20bb3
CHEN Dongmei <DCHEN@lcog.org>
2021-08-17 13:38:54 -0700

    reorganized the script

--- data/Analysis4AppendixF.py
+++ data/Analysis4AppendixF.py
@@ -1,8 +1,68 @@
 import geopandas as gpd
 import os
+import pandas as pd
 
 path = r'T:\MPO\RTP\FY20 2045 Update\Data and Resources\Data\GISData\Updated'
 datapath = r'T:\MPO\RTP\FY20 2045 Update\Data and Resources\Data\ForAppendixF'
+
+categories = ['Auto']*7 + ['Transit']*2 + ['Bike/Ped']*4
+
+project_types = ['Added Freeway Lanes or Major Interchange Improvements',
+                 'Arterial Capacity Improvements',
+                 'New Arterial Link or Interchange',
+                 'New Collectors',
+                 'Transit Oriented Development Implementation',
+                 'Study',
+                 'Urban Standards',
+                 'Frequent Transit Network',
+                 'Stations',
+                 'Multi-Use Paths Without Road Project',
+                 'Multi-Use Paths With Road Project',
+                 'On-Street Lanes or Routes With Road Project',
+                 'On-Street Lanes or Routes Without Road Project']
+
+# combine the same environmental factor
+def combine_RTP_for_each_env_factor(folder='Historic',
+                                    file='NationalRegisterHistoricSitesCLMPO.shp'):
+    df = pd.DataFrame(data={'Project Category': categories, 'Project Type': project_types})
+    if 'HistoricSites' in file:
+        var = 'Historic Sites'
+    df[var] = 0
+    # rwdf: roadway dataframe
+    rwdf = combine_RTP_by_mode(mode='roadway', folder=folder, file=file)
+    if rwdf.shape[0] != 0:
+        for ind in list(rwdf.index):
+            df.loc[df['Project Type'] == ind, var] = rwdf.loc[ind, 'geometry']
+    # bpdf: bike/ped dataframe
+    bpdf = combine_RTP_by_mode(mode='bikeped', folder=folder, file=file)
+    if bpdf.shape[0] != 0:
+        for ind in list(bpdf.index):
+            df.loc[df['Project Type'] == ind, var] = bpdf.loc[ind, 'geometry']
+    # ftndf: frequent transit network dataframe
+    ftndf = RTP_counted_by_intersection(shapefile='FrequentTransitNetwork', 
+                                        folder=folder, file=file,
+                                        transit=True, ftn=True)
+    df.loc[df['Project Type'] == 'Frequent Transit Network', var] = ftndf.shape[0]
+    # stadf: stations dataframe
+    stadf = RTP_counted_by_intersection(shapefile='stations',
+                                        folder=folder, file=file, transit=True)
+    df.loc[df['Project Type'] == 'Stations', var] = stadf.shape[0]
+    return df
+
+# combine the same RTP project categories by roadway or bike/ped
+def combine_RTP_by_mode(mode='roadway', folder='Historic',
+                                file='NationalRegisterHistoricSitesCLMPO.shp'):
+    if mode == 'roadway':
+        df1=RTP_counted_by_intersection(shapefile='Roadway_lines', folder=folder, file=file)
+        df2=RTP_counted_by_intersection(shapefile='Roadway_points', folder=folder, file=file)
+    elif mode == 'bikeped':
+        df1=RTP_counted_by_intersection(shapefile='BikePed', folder=folder, file=file)
+        df2=RTP_counted_by_intersection(shapefile='BikePed_points', folder=folder, file=file)
+        
+    if df2.shape[0] != 0:
+        for ind in list(df2.index):
+            df1.loc[ind, 'geometry'] = df1.loc[ind, 'geometry'] + df2.loc[ind, 'geometry']
+    return df1
 
 # the results should be the same as the output from Select by Location in ArcGIS Pro
 def RTP_counted_by_intersection(shapefile='Roadway_lines',
